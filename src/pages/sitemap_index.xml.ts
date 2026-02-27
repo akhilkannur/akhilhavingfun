d436a8a49ddb9b2b063b5cf7499a7a1a888579b9
akhilkannur <akhil.1000what@gmail.com>
2026-02-27 14:53:28 +0530

    fix: make sitemaps dynamic and add sitemap-main.xml for GSC bypass

--- src/pages/sitemap_index.xml.ts
+++ src/pages/sitemap_index.xml.ts
@@ -1,7 +1,5 @@
 import type { APIRoute } from 'astro';
 
-export const prerender = true;
-
 export const GET: APIRoute = async () => {
   const baseUrl = 'https://akhilhaving.fun';
   const today = new Date().toISOString().split('T')[0];
