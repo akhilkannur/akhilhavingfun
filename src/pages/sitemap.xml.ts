9dc39e03eeacfbdf6595cac630068f8cd6595c3d
akhilkannur <akhil.1000what@gmail.com>
2026-03-02 11:18:09 +0530

    fix: explicit Content-Length for sitemap to assist GSC fetch

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -35,10 +35,12 @@ export const GET: APIRoute = async () => {
   ].join('');
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urlElements}</urlset>`;
+  const bytes = new TextEncoder().encode(xml);
 
-  return new Response(xml, {
+  return new Response(bytes, {
     headers: {
       'Content-Type': 'application/xml',
+      'Content-Length': bytes.length.toString(),
     },
   });
 };
